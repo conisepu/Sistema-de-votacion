1c8661981eb7e2bf9e05f4e0ec78de4dc937211a
conisepu <48888601+conisepu@users.noreply.github.com>
2021-05-12 13:45:28 -0400

    Update menu_CEIND.php

--- SisDeVotacion/menu_CEIND.php
+++ SisDeVotacion/menu_CEIND.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Menu CEIND</title>
-        <link rel="stylesheet" href="css/estilos_menuCEIND.css">
+        <link rel="stylesheet" href="css/estilos_editEncuesta.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
@@ -55,36 +55,37 @@
                 <div class="linea3"></div>
             </div>
         </nav>
-<div class="container p-5">
-	<div class="card">
-		<div class="card-body">
-			<form id="manage_survey">
-				<input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
-				<div class="row">
-					<div class="col-md-8 mx-auto ">
-						<div class="form-group">
-							<label for="" class="control-label">Titulo</label>
-							<input type="text" name="title" class="form-control form-control-sm" required value="<?php echo isset($stitle) ? $stitle : '' ?>">
-						</div>
-						<div class="form-group">
-							<label for="" class="control-label">Inicio</label>
-							<input type="date" name="start_date" class="form-control form-control-sm"  required value="<?php echo isset($start_date) ? $start_date : '' ?>">
-						</div>
-						<div class="form-group">
-							<label for="" class="control-label">Fin</label>
-							<input type="date" name="end_date" class="form-control form-control-sm" required value="<?php echo isset($end_date) ? $end_date : '' ?>">
-						</div>
-					</div>
-				</div>
-				<hr>
-				<div class="col-lg-12 text-right justify-content-center d-flex">
-					<button   class="btn btn-primary mr-2">Guardar</button>
-					<button class="btn btn-secondary" type="button" onclick="location.href = 'lista.php'">Cancelar</button>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
+        <!-- ///////////////////////////////////////////////////////////////////////////////////// -->
+        <div class="container p-5">
+            <div class="card">
+                <div class="card-body">
+                    <form id="manage_survey">
+                        <input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
+                        <div class="row">
+                            <div class="col-md-8 mx-auto ">
+                                <div class="form-group">
+                                    <label for="" class="control-label">Titulo</label>
+                                    <input type="text" name="title" class="form-control form-control-sm" required value="<?php echo isset($stitle) ? $stitle : '' ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="" class="control-label">Inicio</label>
+                                    <input type="date" name="start_date" class="form-control form-control-sm"  required value="<?php echo isset($start_date) ? $start_date : '' ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="" class="control-label">Fin</label>
+                                    <input type="date" name="end_date" class="form-control form-control-sm" required value="<?php echo isset($end_date) ? $end_date : '' ?>">
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="col-lg-12 text-right justify-content-center d-flex">
+                            <button   class="btn btn-primary mr-2">Guardar</button>
+                            <button class="btn btn-secondary" type="button" onclick="location.href = 'lista.php'">Cancelar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 <script  src="ajax.js"> </script>
 <?php include 'footer.php' ?>
 </html>
