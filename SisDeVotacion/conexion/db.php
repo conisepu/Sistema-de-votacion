de047cd21d703bfe053acb0da6ede59031320d0e
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-05-24 18:25:15 -0400

    xd

--- SisDeVotacion/conexion/db.php
+++ SisDeVotacion/conexion/db.php
@@ -3,7 +3,7 @@
 //$conn= new mysqli('sistemavotacion.mysql.database.azure.com','votaciones_db','admi@sistemavotacion','sistemavotacion123.')or die("Could not connect to mysql".mysqli_error($con));
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "//direccion del archivo", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "file:///C:/certificadoSSL/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'sistemavotacion.mysql.database.azure.com', 'admi@sistemavotacion', 'sistemavotacion123.', 'votaciones_db', 3306, MYSQLI_CLIENT_SSL);
 
 ?>
