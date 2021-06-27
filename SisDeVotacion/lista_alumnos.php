e57c6def55f5ce32756bce96d89f0bc6610a93ab
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-06-26 22:14:13 -0400

    solo excel

--- SisDeVotacion/lista_alumnos.php
+++ SisDeVotacion/lista_alumnos.php
@@ -25,7 +25,7 @@
     <p class="card-text">Seleccione un archivo de excel que contenga columnas con los siguientes nombres: RUT, Nombre Estudiante, Año Ingreso, Sexo y Correo UDP.</p>
     <p class="card-text">Recordar que primero se debe subir el excel y luego crear la votacion.</p>
     <form action="#" method="POST" enctype="multipart/form-data">
-        <input type="file" name="excel">
+        <input id="campoFile" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" type="file" name="excel">
         <input type="submit" name="submit">
     </form>
   </div>
@@ -129,6 +129,7 @@
             }
         }
     }
+
 
 ?>
 </body>
